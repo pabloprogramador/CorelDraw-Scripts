4d5386f49ca86c17416ba4c6a5f39b52dd8f83a2
Pablo <pabloprogramador@gmail.com>
2025-06-23 21:25:03 +0100

    link

--- RemoveLine.js
+++ RemoveLine.js
@@ -3,3 +3,5 @@ let OrigSelection = host.ActiveSelectionRange;
 for (let i = 1; i <= OrigSelection.Count; i++) {
     OrigSelection.Item(i).Outline.SetNoOutline();
 }
+
+//http://www.corelclub.org/descargas/GUIA-PROGRAMACION-MACROS-EN-CORELDRAW.pdf
